e8e4e7acfca52799f3899d8c748be07fe0deca74
chartaseus <90235896+chartaseus@users.noreply.github.com>
2024-01-16 16:16:21 +0700

    Create functions for basic calculator operations

---
+++ script.js
@@ -0,0 +1,17 @@
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const multiply = function(a, b) {
+	return a * b;
+};
+
+const divide = function(a, b) {
+  // TODO: round the result if more than display limit. Algorithm: convert
+  // result to string, calculate length, use toFixed if length > display.
+	return a / b;
+};
